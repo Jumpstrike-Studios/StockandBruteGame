b91fcae7e3c724df8a95f0ad6b74114e8f19778b
cris-tofer <chrisnf05@gmail.com>
2024-10-09 14:36:38 -0400

    Optimized the movement script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -21,50 +21,40 @@ public class Movement : MonoBehaviour
 // Update is called once per frame
 void Update()
     {
-        move = Input.GetAxis("Horizontal");
-        if (isGrounded && !Input.GetKeyDown("space")) // Checks to see if player is grounded and if the jump button is not pressed
+        if (Input.GetKey("d"))
         {
-            doubleJump = false;
+            move = 1;
+        }
+        else if (Input.GetKey("a"))
+        {
+            move = -1;
+        }
+        else {
+            move = 0;
         }
 
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
-
+        //Jump and double jump mechanic
         if (Input.GetKeyDown("space"))
         {
             if (isGrounded || doubleJump) // Checks if player is grounded then if doubleJump is true
             {
-               if (doubleJump == false)
-                {
-                    rb.velocity = Vector2.up * jump;
-                }
-               if (doubleJump == true)
-                {
-                    rb.velocity = Vector2.up * jump;
-                }
-
-               doubleJump = !doubleJump;
-            }                                
+                rb.velocity = Vector2.up * jump;
+                doubleJump = !doubleJump;
+                isGrounded = false;
+            }
         }
         if (Input.GetKeyUp("space") && rb.velocity.y > 0f) // If space is let go mid-jump, upwards velocity halved for smaller jump
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
-
     }
 void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
-        }
-    }
-
-void OnCollisionExit2D(Collision2D collision)
-    { 
-        if (collision.gameObject.CompareTag("Ground")) // When collision with the ground is lost, doubleJump is true, enabling another jump
-        {
-            isGrounded = false;
-            doubleJump = true;
+            doubleJump = false;
         }
     }
 }
