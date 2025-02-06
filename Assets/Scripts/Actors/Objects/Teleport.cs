385e071f914a024c1a8b4f6266c422ddf0d712f1
Shadowshield78 <devondicamillo15@gmail.com>
2025-02-06 17:08:01 -0500

    Teleport Function

---
+++ Assets/Scripts/Actors/Objects/Teleport.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public CapsuleCollider2D capcol;
+
+    public GameObject player;
+    public Actor_Player actor;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        actor = player.GetComponent<Actor_Player>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            player.transform.position = new Vector3(-2.75f, 71.2f, 0);
+        }
+    }
+}
