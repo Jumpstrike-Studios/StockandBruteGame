771df9ac287c0b3a988813ea10bf4b2823fa2fae
realfruit08 <mick.mcnulty08@gmail.com>
2025-02-13 18:39:40 -0500

    background update pending

--- Assets/Sprites/Parallax Background/Paralax.cs
+++ Assets/Sprites/Parallax Background/Paralax.cs
@@ -9,14 +9,15 @@ public class Paralax : MonoBehaviour
 
     // Start is called once before the first execution of Update
     void Start()
-    { length = GetComponent<SpriteRenderer>().bounds.size.x;
+    { 
+      length = GetComponent<SpriteRenderer>().bounds.size.x;
       spriteStartPos = transform.position.x;
     }
     void FixedUpdate()
     {
        float moving = (cam.transform.position.x * paralax);
        transform.position = new Vector3(spriteStartPos + moving, transform.position.y, transform.position.z);
-        float temp = cam.transform.position.x * (1 + paralax);
+       float temp = cam.transform.position.x * (1 + paralax);
       
 
     }
